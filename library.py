be83a80381413eeb0b518dd399aaecfa738cd749
Xayan Kyle Lovell <xayanklovell@gmail.com>
2026-07-28 23:18:02 +0200

    I implemented the add_book function

--- library.py
+++ library.py
@@ -14,15 +14,35 @@ def most_borrowed(books):
 
 # Asks for a number of copies, validates with try-except, returns int or None
 def read_valid_copies():
-    pass
+    while True:
+        try:
+            copies = int(input("Enter the number of copies: "))
+            if copies <= 0:
+                print("Copies must be greater than 0.")
+            else:
+                return copies
+        except ValueError:
+            print("Invalid number. Please enter a whole number.")
 
-
-# Adds a new book OR adds copies to an existing title by the same author
+    
+#Adds a new book OR adds copies to an existing title by the same author
 def add_book(books):
     global next_book_number
     title = input("Enter the book title: ")
-    author = input("Enter the author: ")
-    copies = int(input("Enter the number of copies: "))
+    author = input ("Enter the author: ")
+    copies = read_valid_copies()
+
+    #Searches for every existing book first
+    for book_id in books:
+        book = books[book_id]
+        if book ["title"] == title and book["author"] == author:
+            if book["title"] == title and book["author"] == author:
+                book["total"] = book["total"] + copies
+                book["available"] = book["available"] + copies
+            print("Book already exist! Copies have been updated.")
+            return
+
+    #If we never found the book create a brand new one 
     book_id = "B" + str(next_book_number)
     books[book_id] = {
         "title": title,
@@ -32,10 +52,13 @@ def add_book(books):
         "times_borrowed": 0
     }
     next_book_number = next_book_number + 1
-    print (books)
+    print("New book added!")
+    print(books)
+
+    
 # Registers a new member with an empty borrowed list
 def register_member(members):
-    pass
+    global next_member_number
 
 
 # One member borrows one book - enforces ALL the rules, updates BOTH dicts
